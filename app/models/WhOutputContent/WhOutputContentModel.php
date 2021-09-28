93dd9e1c3e1344ca1d8c24dddb147fcefba7301e
juarezjorr <juarezjorr@gmail.com>
2021-09-27 23:06:18 -0500

    repositorio27

--- app/models/WhOutputContent/WhOutputContentModel.php
+++ app/models/WhOutputContent/WhOutputContentModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No se permite acceso directo');
 
-class ProductsForSublettingModel extends Model
+class WhOutputContentModel extends Model
 {
 
     public function __construct()
@@ -9,15 +9,29 @@ class ProductsForSublettingModel extends Model
       parent::__construct();
     }
 
+    // Listado de proyectos
+    public function listProjects($store)
+    {
+        $store = $this->db->real_escape_string($store);
+        $qry = "SELECT pt.pjttp_name, pj.pjt_name, pj.pjt_number,
+        DATE_FORMAT(pj.pjt_date_start,'%d/%m/%Y') AS pjt_date_start, 
+        DATE_FORMAT(pj.pjt_date_end,'%d/%m/%Y') AS pjt_date_end, 
+        DATE_FORMAT(pj.pjt_date_project,'%d/%m/%Y %H:%i ') AS pjt_date_project, 
+        pj.pjt_location, pj.cuo_id, '1' as analyst, '33' as freelance, pj.pjt_id
+       FROM ctt_projects AS pj INNER JOIN ctt_location AS lo ON lo.loc_id = pj.loc_id 
+       LEFT JOIN ctt_projects_type As pt ON pt.pjttp_id = pj.pjttp_id 
+       WHERE pj.pjt_id='1' ORDER BY pjt_date_start ASC";
+        return $this->db->query($qry);
+    }    
+
 // Listado de Productos
-    public function listProducts($store)
+    public function listDetailProds($store)
     {
         $store = $this->db->real_escape_string($store);
         $qry = "SELECT 
-                    p.prd_id, p.prd_name, p.prd_sku, ifnull(s.ser_id,0) as ser_id, ifnull(s.ser_serial_number,'') as ser_serial_number, ifnull(s.ser_cost,'') as ser_cost, ifnull(p.cin_id,'0') as cin_id
-                FROM ctt_products AS p
-                LEFT JOIN ctt_series AS s ON s.prd_id = p.prd_id AND s.ser_behaviour = 'R'
-                WHERE p.prd_status = 1 AND prd_visibility = 1 ORDER BY p.prd_name asc ;";
+        pd.pjtpd_id, pd.pjtdt_prod_sku, pd.pjtdt_prod_name, pd.pjtdt_quantity, pd.ser_id,pd.pjtcn_id 
+        FROM ctt_projects_content AS pc INNER JOIN ctt_projects_detail AS pd 
+        ON pd.pjtcn_id = pc.pjtcn_id WHERE pc.pjt_id = 1 order by 1,4;";
         return $this->db->query($qry);
     }    
 
@@ -27,14 +41,6 @@ class ProductsForSublettingModel extends Model
     {
         $store = $this->db->real_escape_string($store);
         $qry = "SELECT sup_id, sup_business_name FROM ctt_suppliers WHERE sup_status = 1 AND sut_id in (3) ORDER BY sup_business_name;";
-        return $this->db->query($qry);
-    }   
-
-
-// Listado de monedas
-    public function listCoins()
-    {
-        $qry = "SELECT * FROM ctt_coins WHERE cin_status = 1;";
         return $this->db->query($qry);
     }   
 
--- app/views/WhOutputContent/WhOutputContent.php
+++ app/views/WhOutputContent/WhOutputContent.php
@@ -1,124 +1,143 @@
 <?php 
-	defined('BASEPATH') or exit('No se permite acceso directo'); 
-	require ROOT . FOLDER_PATH . "/app/assets/header.php";
+    defined('BASEPATH') or exit('No se permite acceso directo'); 
+    require ROOT . FOLDER_PATH . "/app/assets/header.php";
 ?>
 
 <header>
-	<?php require ROOT . FOLDER_PATH . "/app/assets/menu.php"; ?>
+    <?php require ROOT . FOLDER_PATH . "/app/assets/menu.php"; ?>
 </header>
 <div class="container-fluid">
-		<div class="contenido ">
-			<div class="row mvst_group">
-				<div class="mvst_panel">
-					<div class="form-group">
+    <div class="contenido">
+            
+        <div class="row mvst_group">
 
+
+            <div class="mvst_panel">
+                <div class="form-group">
+                <div class="form_primary">
+                    <h4 class="mainTitle">Datos de los Proyectos</h4>
+
+                        <div class="row">
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtTipoProject" type="text" class="form-control form-control-sm" >
+                                <label for="txtTipoProject">Tipo de Proyecto</label>
+                            </div>
+                           
+                        </div>
+						<div class="row">
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtProjectName" type="text" class="form-control form-control-sm" >
+                                <label for="txtProjectName">Nombre del Proyecto</label>
+                            </div>
+                           
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtProjectNum" type="text" class="form-control form-control-sm" >
+                                <label for="txtProjectNum">Numero Proyecto</label>
+                            </div>
+                        </div>
 						
-
-						<div class="row list-finder">
-							<div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
-									<input id="txtProducts" type="text" class="form-control form-control-sm required" data-mesage="Debes seleccionar un producto" >
-									<label for="txtProducts">Productos</label>
-									<input type="hidden" id="txtIdProducts" name="txtIdProducts">
-							</div>
-							<div class="list-group list-hide">
-								<div class="list-items" id="listProducts"></div>
-							</div>
-						</div>
 						<div class="row">
 							<div class="col-md-6 col-lg-6 col-xl-6 mb-2 form-floating">
-								<input id="txtStartDate" type="text" class="form-control form-control-sm" placeholder="Fecha inicial">
+								<input id="txtStartDate" type="text" class="form-control form-control-sm">
 								<label for="txtStartDate" >Fecha Incial</label>
 							</div>
 							<div class="col-md-6 col-lg-6 col-xl-6 mb-2 form-floating">
-								<input id="txtEndDate" type="text" class="form-control form-control-sm required" placeholder="Fecha final"  data-mesage="La fecha final debe ser mayor a la fecha de inicio">
+								<input id="txtEndDate" type="text" class="form-control form-control-sm">
 								<label for="txtEndDate">Fecha Final</label>
 							</div>
 						</div>
-
 						<div class="row">
-							<div class="col-md-6 col-lg-6 col-xl-6 mb-2 form-floating">
-							<input id="txtPrice" type="text" class="form-control form-control-sm text-center number required" data-mesage="Debes Agregar el precio" >
-								<label for="txtPrice">Costo subarrendo</label>
-							</div>
-							<div class="col-md-6 col-lg-6 col-xl-6 mb-2 form-floating">
-								<input id="txtQuantity" type="text" class="form-control form-control-sm text-center number required"  data-mesage="Debes agregar la cantidad de productos" >
-								<label for="txtQuantity">Cantidad</label>
-							</div>
-						</div>
-
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtLocation" type="text" class="form-control form-control-sm" >
+                                <label for="txtLocation">Locacion</label>
+                            </div>
+                        </div>
 						<div class="row">
-							<div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
-								<select id="txtCoinType" class="form-select form-select-sm  required" aria-label="Floating label select" data-mesage="Debes seleccionar el tipo de moneda">
-									<option value="0" selected>Selecciona el tipo de moneda</option>
-								</select>
-								<label for="txtCoinType">Tipo de moneda</label>
-							</div>
-						</div>
-
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtCustomer" type="text" class="form-control form-control-sm" >
+                                <label for="txtCustomer">Cliente</label>
+                            </div>
+                        </div>
 						<div class="row">
-							<div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
-								<select id="txtSupplier" class="form-select form-select-sm required" aria-label="Floating label select"  data-mesage="Debes seleccionar un proveedor">
-									<option value="0" selected>Selecciona el proveedor</option>
-								</select>
-								<label for="txtSupplier">Proveedor</label>
-							</div>
-						</div>
-
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtAnalyst" type="text" class="form-control form-control-sm" >
+                                <label for="txtAnalyst">Analista</label>
+                            </div>
+                        </div>
 						<div class="row">
-							<div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
-								<select id="txtStoreSource" class="form-select form-select-sm required"><option value="0" selected>Selecciona almacen</option></select>
-								<label for="txtStoreSource" class="form-label">Almacen</label>
-							</div>
-						</div>
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <input id="txtFreelance" type="text" class="form-control form-control-sm" >
+                                <label for="txtFreelance">Freelance Asignado</label>
+                            </div>
+                        </div>
 
-						<div class="row">
-							<div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
-								<textarea class="form-control form-control-sm" id="txtComments" style="height: 120px" rows="3"></textarea>
-								<label for="txtComments">Comentarios</label>
-							</div>
-						</div>
+                    </div>
 
-						<div class="row">
-							<div class="col-md-8 mb-5">
-								<button type="button" class="btn btn-sm btn-primary disabled" id="btn_subletting">Agregar</button>
-							</div>
-						</div>
+                    <div class="form_secundary">
+                        <h4>Seleccion de productos</h4>
+                        <div class="row">
+                            <input type="hidden" id="txtIdPackages" name="txtIdPackages"><br>
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <select id="txtCategoryProduct" class="form-select form-select-sm required">
+                                    <option value="0" data-content="||||" selected>Selecciona una categoría</option>
+                                </select>
+                                <label for="txtCategoryProduct">Categoria</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <select id="txtSubcategoryProduct" class="form-select form-select-sm required">
+                                    <option value="0" selected>Selecciona una subcategoría</option>
+                                </select>
+                                <label for="txtSubcategoryProduct" class="form-label">Subcategoia</label>
+                            </div>
+                        </div>
 
-					</div>
-				</div>
 
-				<div class="mvst_table">
-					<h1>Productos para subarrendo</h1>
-					<table class="display compact nowrap"  id="tblProductForSubletting">
-						<thead>
-							<tr>
-								<th style="width:  80px">soppr</th>
-								<th style="width:  30px"></th>
-								<th style="width:  120px">SKU</th>
-								<th style="width: 350px">Producto</th>
-								<th style="width:  60px">Cantidad</th>
-								<th style="width:  90px">precio</th>
-								<th style="width: 200px">Proveedor</th>
-								<th style="width:  90px">Fecha Inicio</th>
-								<th style="width:  90px">Fecha Fin</th>
-								<th style="width: 350px">Nota</th>
-							</tr>
-						</thead>
-						<tbody>						
-						</tbody>
-					</table>
-				</div>
-			</div>
-				
-					
-				
-			
-	</div>
+                        <div class="row list-finder">
+                            <div class="col-md-12 col-lg-12 col-xl-12 mb-2 form-floating">
+                                <div class="box-items-list" id="boxProducts">Productos
+                                    <i class="fas fa-angle-down"></i>
+                                </div>
+                            </div>
+                            <div class="list-group list-hide">
+                                <div class="list-items" id="listProducts"></div>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="mvst_table">
+                <h1>Detalles</h1>
+                    <h3>Asignacion de Productos</h3>
+                    <table class="display compact nowrap"  id="tblAsignedProd">
+                        <thead>
+                             <tr>
+                                    <th style="width:  40px"></th>
+                                    <th style="width:  90px">SKU</th>
+                                    <th style="width:  Auto">Descripcion</th>
+                                    <th style="width:  90px">Num. Serie</th>
+									<th style="width:  70px">Cantidad</th>
+									<th style="width:  70px">Tipo de Producto</th>
+                             </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+            </div>           
+        </div>
+    </div>
 </div>
 
 
 <!-- Modal Borrar -->
-<div class="modal fade" id="MoveFolioModal" tabindex="-1" aria-labelledby="BorrarPerfilLabel" aria-hidden="true">
+<div class="modal fade" id="delPackModal" tabindex="-1" aria-labelledby="BorrarPerfilLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-sm">
             <div class="modal-content">
             <div class="modal-header ">
@@ -127,20 +146,48 @@
 
 
             <div class="row">
+                <input type="hidden" class="form-control" id="txtIdPackage" aria-describedby="basic-addon3">
                 <div class="col-12 text-center">
-                    <span class="modal-title text-center" style="font-size: 1.2rem;" id="BorrarPerfilLabel">Folio: <h3 class="resFolio"></h3></span>
+                    <span class="modal-title text-center" style="font-size: 1.2rem;" id="BorrarPerfilLabel">¿Seguro que desea borrarlo?</span>
                 </div>
             </div>
 
             </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" id="btnHideModal">Cerrar</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-danger" id="btnDelPackage">Borrar</button>
                 </div>
             </div>
     </div>
 </div>
 
+
+<div class="modal fade" id="delProdModal" tabindex="-1" aria-labelledby="BorrarPerfilLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm">
+            <div class="modal-content">
+            <div class="modal-header ">
+            </div>
+            <div class="modal-body" style="padding: 0px !important;">
+
+
+            <div class="row">
+                <input type="hidden" class="form-control" id="txtIdProductPack" aria-describedby="basic-addon3">
+                <div class="col-12 text-center">
+                    <span class="modal-title text-center" style="font-size: 1.2rem;" id="BorrarPerfilLabel">¿Seguro que desea borrarlo?</span>
+                </div>
+            </div>
+
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-danger" id="btnDelProduct">Borrar</button>
+                </div>
+            </div>
+    </div>
+</div>
+
+
 <script src="<?=  PATH_ASSETS . 'lib/functions.js' ?>"></script>
-<script src="<?=  PATH_VIEWS . 'ProductsForSubletting/ProductsForSubletting.js' ?>"></script>
+<script src="<?=  PATH_VIEWS . 'Packages/Packages.js' ?>"></script>
 
 <?php require ROOT . FOLDER_PATH . "/app/assets/footer.php"; ?>
